89d4e7544684491d97f2cad0db77ce5ea37eb0d3
Mohamed Magdy <mohamedmagdy7599@gmail.com>
2024-02-02 17:25:31 -0500

    adding signup login validation

--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,53 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 import './Sign_Up.css'
-export default function Sign_Up() {
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
+
+const Sign_Up = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
+
+    const navigate = useNavigate();
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        // API Call
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+
+            }),
+        });
+
+        const json = await response.json();
+
+        if (json.authtoken) {
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            // phone and email
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
+            // Redirect to home page
+            navigate("/");   //on directing to home page you need to give logic to change login and signup buttons with name of the user and logout button where you have implemented Navbar functionality
+            window.location.reload();
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    setShowerr(error.msg);
+                }
+            } else {
+                setShowerr(json.error);
+            }
+        }
+    };
+
     return (
-        <>
-            <div className="container" style={{ marginTop: "5%" }}>
-                <div className='form-wrapper'>
-                    <div className='signup-text'>
-                        <h1>Sign Up</h1>
-                        <div className="signup-text1" style={{ textAlign: "left" }}>
-                            <p>Already a member?</p>
-                            <span
-                            ><Link to="/login" style={{ color: "#2190ff" }}>
-                                    Login</Link
-                                ></span
-                            >
-                        </div>
-                    </div>
-                    <form action="">
+        <div className="container" style={{ marginTop: '5%' }}>
+            <div className="signup-grid">
+                <div className="signup-form">
+                    <form method="POST" onSubmit={register}>
                         <div className="form-group">
-                            <label for="role">Role:</label>
-                            <input type="text" required name="role" id="role" className='form-control' />
+                            <label htmlFor="email">Email</label>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
                         </div>
                         <div className="form-group">
-                            <label for="name">Name:</label>
-                            <input type="text" required name="name" id="name" className='form-control' />
+                            <label htmlFor="name">Name</label>
+                            <input value={name} type="text" onChange={(e) => setName(e.target.value)} name="name" id="name" className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
                         </div>
                         <div className="form-group">
-                            <label for="number" >Phone number:</label>
-                            <input type="text" required pattern="[0-9]{10}" title="Please enter a 10-digit phone number"
-                                name="number" id="number" className='form-control' />
+                            <label htmlFor="phone">Phone</label>
+                            <input value={phone} onChange={(e) => setPhone(e.target.value)} type="tel" name="phone" id="phone" className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
                         </div>
                         <div className="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}" required name="email" id="email" className='form-control' />
-                        </div>
-                        <div className="form-group">
-                            <label for="password">Password:</label>
-                            <input type="password" required name="password" id="password" className='form-control' />
-                        </div>
-                        <div className="btn-group">
-                            <button type="submit" className="signup-btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
-                            <button type="reset" className="signup-btn btn-danger mb-2 waves-effect waves-light">Reset</button>
+                            <label htmlFor="password">Password</label>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
                         </div>
                     </form>
                 </div>
-            </div >
-        </>
-    )
+            </div>
+        </div>
+        //Sign up role is not stored in database. You can apply logic for this according to your react code.
+    );
 }
 
-
+export default Sign_Up;
